061c9d44c43a6f01c1966f196b79e1da7ca90804
Estebandido123 <este.toledo@duocuc.cl>
2024-12-05 17:35:57 -0300

    asd

--- RefugioAndino/settings.py
+++ RefugioAndino/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o=94^5%ij_x75s@#rsj((h3#4@lvv@l@zk386$mwbmgn=+9c$q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
